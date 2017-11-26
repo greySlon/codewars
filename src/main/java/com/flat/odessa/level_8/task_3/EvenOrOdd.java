9da9fa86ad7cf4948e324b72c1af9b43512cd7f7
Sergii.Garanenko <garanenkosergii@gmail.com>
2017-11-26 21:21:53 +0200

    even_or_odd

---
+++ src/main/java/com/flat/odessa/level_8/task_3/EvenOrOdd.java
@@ -0,0 +1,11 @@
+package com.flat.odessa.level_8.task_3;
+
+/**
+ * https://www.codewars.com/kata/even-or-odd
+ */
+public class EvenOrOdd {
+
+  public static String even_or_odd(int number) {
+    return number % 2 == 0 ? "EVEN" : "ODD";
+  }
+}
